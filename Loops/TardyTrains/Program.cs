494a9d8e2bc4cb4244f44e040b37270f247db913
Ryan Roice <ryanroice25@gmail.com>
2024-10-21 20:09:58 +0100

    Tardy Trains exercise completed

---
+++ Loops/TardyTrains/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Tardy Train Monitoring Application");
+
+int minutesLate;
+int totalTrains = -1;
+int trainsLate = 0;
+int mostLate = 0;
+
+do
+{
+    totalTrains += 1;
+    Console.WriteLine("How many minutes was the train late?");
+    minutesLate = int.Parse(Console.ReadLine());
+    if (minutesLate > 0)
+    {
+        trainsLate += 1;
+        if(minutesLate > mostLate)
+        {
+            mostLate = minutesLate;
+        }
+    }
+} while(minutesLate >= 0);
+
+Console.WriteLine(trainsLate + " out of " + totalTrains + " trains were late.");
+Console.WriteLine("The latest train was " + mostLate + " minutes late.");
